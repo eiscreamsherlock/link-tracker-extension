971684bc588b3666dce886cb1433b3a343028b36
eiscreamsherlock <>
2026-05-11 23:30:43 +0200

    refactoring some bits

--- index.js
+++ index.js
@@ -4,12 +4,34 @@ const inputBtn = document.querySelector("#input-btn");
 const clearBtn = document.querySelector("#clear-btn");
 const ulEl = document.querySelector("#ul-el");
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if(leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads(myLeads);
+}
+
+function renderLeads(leads) {
+  let listItems = "";
+  // renderList = JSON.parse(localStorage.getItem(`${leads}`));
+  for (let i = 0; i < leads.length; i++) {
+    thisItem = leads[i];
+    listItems += `
+      <li>
+        <a href='${thisItem}' target='_blank'>
+          ${thisItem}
+        </a>
+      </li>`;
+  };
+  ulEl.innerHTML = listItems;
+}
+
 inputBtn.addEventListener("click", function() {
   if (inputEl.value) {
     myLeads.push(inputEl.value);
     inputEl.value = '';
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderLeads();
+    renderLeads(myLeads);
   }
 });
 
@@ -22,43 +44,10 @@ inputEl.addEventListener("keypress", function(event) {
   }
 });
 
-clearBtn.addEventListener("click", function() {
+clearBtn.addEventListener("dblclick", function() {
   if (localStorage.getItem("myLeads")) {
     localStorage.clear();
-    ulEl.innerHTML = '';
+    myLeads = [];
+    renderLeads(myLeads);
   }
 });
-
-function renderLeads() {
-  let listItems = "";
-  renderList = JSON.parse(localStorage.getItem("myLeads"));
-  for (let i = 0; i < renderList.length; i++) {
-    thisItem = renderList[i];
-    listItems += `
-      <li>
-        <a href='${thisItem}' target='_blank'>
-          ${thisItem}
-        </a>
-      </li>`;
-  };
-  ulEl.innerHTML = listItems;
-}
-
-
-renderLeads();
-
-// function renderLeads() {
-//   let listItems = "";
-//   renderList = JSON.parse(localStorage.getItem("myLeads"));
-//   // for (let i = 0; i < myLeads.length; i++) {
-//   for (let i = 0; i < renderList.length; i++) {
-//     thisItem = myLeads[i];
-//     listItems += `
-//       <li>
-//         <a href='${myLeads[i]}' target='_blank'>
-//           ${myLeads[i]}
-//         </a>
-//       </li>`;
-//   };
-//   ulEl.innerHTML = listItems;
-// }
